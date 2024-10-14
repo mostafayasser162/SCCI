8f684397f3822513c230710810c2227660ae4520
Mostafa Yasser <135142328+mostafayasser162@users.noreply.github.com>
2024-10-14 19:10:20 +0300

    Add files via upload

---
+++ scci-website 2/g2.js
@@ -0,0 +1,18 @@
+var typed5 = new Typed('.element', {
+    strings: ["Gallery'22", "Gallery'22"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    cursorChar: ' ',
+    shuffle: true,
+    smartBackspace: false,
+    loop: false
+  });
+
+///--------------//
+
+
+
+
+
+  /*-------slider for mobile*/
+  
